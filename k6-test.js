667a2c88e21105830dccf751f1263259681816d9
Shankysai2601 <sankarit1994@gmail.com>
2020-12-20 16:24:12 +0000

    Testing commit

--- k6-test.js
+++ k6-test.js
@@ -2,7 +2,7 @@ import { sleep } from 'k6';
 import http from 'k6/http';
 
 export let options = {
-  duration: '20s',
+  duration: '30s',
   vus: 50,
   thresholds: {
     http_req_duration: ['p(95)<500'], // 95 percent of response times must be below 500ms
